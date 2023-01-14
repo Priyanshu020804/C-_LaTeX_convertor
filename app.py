699d8cf18afc1a17b20d859f6c0c48fed5ee4411
FRANKENSTEIN0211 <98984204+FRANKENSTEIN0211@users.noreply.github.com>
2023-01-14 21:41:34 +0530

    complete project

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def cpp_to_latex():
+    if request.method == 'POST':
+        code = request.form['code']
+        latex_code = '\\begin{minted}[bgcolor=gray!30,fontsize=\Large,rulecolor=\color{blue},linenos,escapeinside=||,xleftmargin=5mm,xrightmargin=5mm,numbersep=5mm,frame=lines,framesep=2mm,fontfamily=tt]{c++}\n' + code + '\n\\end{minted}'
+        # latex_code = latex_code.replace('\n', '<br>')
+        return render_template('index.html', latex_code=latex_code)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
