a4aa0de0f1464261de2d4fe17bc790a6431737f0
FRANKENSTEIN0211 <98984204+FRANKENSTEIN0211@users.noreply.github.com>
2023-01-14 23:08:18 +0530

    final update

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ app = Flask(__name__)
 def cpp_to_latex():
     if request.method == 'POST':
         code = request.form['code']
-        latex_code = '\\begin{minted}[bgcolor=gray!30,fontsize=\Large,rulecolor=\color{blue},linenos,escapeinside=||,xleftmargin=5mm,xrightmargin=5mm,numbersep=5mm,frame=lines,framesep=2mm,fontfamily=tt]{c++}\n' + code + '\n\\end{minted}'
-        # latex_code = latex_code.replace('\n', '<br>')
+        latex_code = '\\documentclass{article}\n'+'\\usepackage{minted}\n'+'\\begin{document}\n'+'\\begin{minted}[bgcolor=gray!30,fontsize=\Large,rulecolor=\color{blue},linenos,escapeinside=||,xleftmargin=5mm,xrightmargin=5mm,numbersep=5mm,frame=lines,framesep=2mm,fontfamily=tt]{c++}\n' + code + '\n\\end{minted}'+ '\n\\end{document}'
         return render_template('index.html', latex_code=latex_code)
     return render_template('index.html')
 
